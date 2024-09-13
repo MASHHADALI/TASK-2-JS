3f708d1809f659cabab55f91070620056222d8f6
Mashhad Ali Shah <165510733+MASHHADALI@users.noreply.github.com>
2024-09-13 09:33:32 +0500

    Add files via upload

---
+++ TASK2.js
@@ -0,0 +1,85 @@
+// TASK-1
+// let operator = prompt("enter an operator (either +-*or/)")
+// let no = parseFloat(prompt("enter first no"))
+// let no2 = parseFloat(prompt("enter second no"))
+
+// if (operator== "+"){alert(no + no2)}
+// else if (operator== "-"){alert(no - no2)}
+// else if (operator== "*"){alert(no * no2)}
+// else  {alert(no / no2)}
+// console.log(`${no} ${operator} ${no2} = ${alert}`)
+
+
+
+// TASK-2
+// let name = prompt("enter your name")
+// let color = prompt ("enter your favourite color")
+// alert("hello"+ " "+name +  " i see your favourite color is " + " " +color)
+
+
+// TASK-3
+// let no = prompt("enter your number")
+// if(no %2 == 0){alert("even number")}
+// else(alert("odd number"))
+
+
+
+// // TASK-4
+// let  arr = ["banana" , "mango" , "apple" , "strawberry", "guava"]
+// // adding name to last
+// arr.push("kivi")
+// // adding name to 1sst
+// arr.unshift("pomegronate")
+// // removing elemnt from last
+// arr.pop()
+// // removing element from 1st
+// arr.shift()
+// alert( arr)
+
+
+// TASK-5
+// let score = 0;
+// let question1 = prompt("what is 2+2?")
+// if (question1 == "4"){score++}
+
+// let question2 = prompt("what is the capital of japan?")
+// if (question2 == "tokyo"){score++}
+
+// let question3 = prompt("who is the greatest player of football?")
+// if (question3 == "ronaldo"){score++}
+// alert("you got " +score +" " + "right")
+
+
+
+
+// // TASK-6
+// let marks = prompt("enter marks")
+// if (marks ==100){alert("your grade is A+")}
+// else if (marks >=90){alert("your grade is A")}
+// else if (marks >=80){alert("your grade is B+")}
+// else if (marks >=70){alert("your grade is B")}
+// else if (marks >=60){alert("your grade is C+")}
+// else if (marks >=50){alert("your grade is C")}
+// else(alert("fail"))
+
+
+// TASK-7
+// for (let num = 10 ; num>=0; num--)
+// {console.log (num)}
+// alert("completed")
+
+
+
+
+// TASK-8
+// let arr =["1","2","3","4","5","6","7","8","9","10"]
+
+// arr.shift()
+// arr.pop()
+// arr.unshift("one")
+// arr.push("ten")
+// // arr.splice
+// let newarr= Math.floor(arr.length / 2)
+// let newelement = ["x","y"]
+// arr.splice(newarr , 0 , ...newelement )
+// console.log(arr)
